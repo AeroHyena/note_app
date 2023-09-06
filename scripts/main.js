a0d8be8e9e3d76c81e8247738ccfabb5e38d28e9
Arnold Lohmann <rakudreemurr@gmail.com>
2023-09-06 06:10:28 +0000

    V1.1.0 - added the ability to edit the content of the title and note itself. No functionality to store notes is present at this stage, so changes are not saved permanently. Updated the main.js script to contain used functions in place of importing said functions from other modules.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 /* 
 This is the main script used in the app.
-It consolidates the multi-use scripts into one main script
-that is used to supplement and provide functionality within the note app.
+It contains any functions and logic that is used
+to provide functionality to the app.
 */
+
+
+// Make a function that allows the user to edit the content of the applied area.
+// Tgis function is intended ot be invoked via an onclick event listener. 
+function edit_contents(id) {
+    let content;
+
+    content = document.getElementById(id);
+    console.log(content);
+    content.contentEditable = true;
+    content.focus();
+}
